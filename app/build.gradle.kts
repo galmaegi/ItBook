bf0ec27f30d6e2bf1f174d5d96467ad4160f341d
KyoungMin <rlaudals2374@gmail.com>
2021-07-24 19:47:12 +0900

    Split libraries dependencies into buildSrc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,13 +6,13 @@ plugins {
 }
 
 android {
-    compileSdk = 30
-    buildToolsVersion = "30.0.3"
+    compileSdk = Sdk.COMPILE_SDK_VERSION
+    buildToolsVersion = Sdk.BUILD_TOOL_SDK_VERSION
 
     defaultConfig {
         applicationId = "com.example.itbook"
-        minSdk = 21
-        targetSdk = 30
+        minSdk = Sdk.MIN_SDK_VERSION
+        targetSdk = Sdk.TARGET_SDK_VERSION
         versionCode = 1
         versionName = "1.0"
 
@@ -60,40 +60,32 @@ dependencies {
 
     implementation(project(":ItBookApi"))
 
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
-    implementation("com.github.bumptech.glide:glide:4.12.0")
-    implementation("androidx.room:room-runtime:2.3.0")
-    implementation("androidx.room:room-ktx:2.3.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
+    implementation(SupportLibs.ANDROID_MATERIAL)
+    implementation(SupportLibs.ANDROIDX_CORE_KTX)
+    implementation(SupportLibs.ANDROIDX_APPCOMPAT)
+    implementation(SupportLibs.ANDROIDX_CONSTRAINT_LAYOUT)
+    implementation(SupportLibs.ANDROIDX_LIFECYCLE_LIVEDATA)
+    implementation(SupportLibs.ANDROIDX_LIFECYCLE_VIEWMODEL)
+    implementation(SupportLibs.ANDROIDX_NAVIGATION_FRAGMENT_KTX)
+    implementation(SupportLibs.ANDROIDX_NAVIGATION_UI_KTX)
+    implementation(SupportLibs.ANDROIDX_ACTIVITY_KTX)
+    implementation(SupportLibs.ANDROIDX_FRAGMENT_KTX)
+    implementation(SupportLibs.ANDROIDX_FRAGMENT_KTX)
 
-    kapt("androidx.room:room-compiler:2.3.0")
+    implementation(KotlinCoroutine.ANDROID)
+    implementation(KotlinCoroutine.CORE)
 
-    implementation("androidx.work:work-runtime-ktx:2.5.0")
-    implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("androidx.fragment:fragment-ktx:1.3.5")
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.activity:activity-ktx:1.2.3")
+    implementation(Glide.GLIDE)
 
-    implementation("com.google.dagger:hilt-android:2.37")
-    kapt("com.google.dagger:hilt-android-compiler:2.37")
-    kapt("com.google.dagger:hilt-compiler:2.37")
-    kapt("androidx.hilt:hilt-compiler:1.0.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.arch.core:core-testing:2.1.0")
-    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.0")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    implementation(Hilt.HILT_ANDROID)
+    kapt(Hilt.HILT_ANDROID_COMPILER)
+    kapt(SupportLibs.ANDROIDX_HILT_COMPILER)
+
+    testImplementation(JUnit.JUNIT)
+    androidTestImplementation(KotlinCoroutine.TEST)
+    androidTestImplementation(SupportLibs.ANDROIDX_TEST_ESPRESSO_CORE)
+    androidTestImplementation(SupportLibs.ANDROIDX_ARCH_CORE_TESTING)
+    androidTestImplementation(SupportLibs.ANDROIDX_TEST_JUNIT)
 }
 
 kapt {
---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,95 @@
+object Sdk {
+    const val MIN_SDK_VERSION = 21
+    const val TARGET_SDK_VERSION = 30
+    const val COMPILE_SDK_VERSION = 30
+    const val BUILD_TOOL_SDK_VERSION = "30.0.3"
+}
+
+object Versions {
+    const val ANDROID_MATERIAL = "1.4.0"
+
+    const val ANDROIDX_CORE = "1.6.0"
+    const val ANDROIDX_APPCOMPAT = "1.3.1"
+    const val ANDROIDX_CONSTRAINT_LAYOUT = "2.0.4"
+    const val ANDROIDX_LIFECYCLE = "2.3.1"
+    const val ANDROIDX_ROOM = "2.3.0"
+    const val ANDROIDX_NAVIGATION = "2.3.5"
+    const val ANDROIDX_ACTIVITY = "1.2.3"
+    const val ANDROIDX_FRAGMENT = "1.3.6"
+    const val ANDROIDX_HILT = "1.0.0"
+    const val ANDROIDX_ESPRESSO = "3.4.0"
+    const val ANDROID_ARCH_CORE = "2.1.0"
+    const val ANDROIDX_TEST_JUNIT = "1.1.3"
+
+
+    const val GLIDE = "4.12.0"
+    const val RETROFIT = "2.9.0"
+    const val OKHTTP3 = "4.9.1"
+    const val KOTLIN_COROUTINE = "1.5.0"
+    const val HILT = "2.37"
+
+    const val JUNIT = "4.13.2"
+}
+
+object SupportLibs {
+    const val ANDROID_MATERIAL = "com.google.android.material:material:${Versions.ANDROID_MATERIAL}"
+
+    const val ANDROIDX_CORE_KTX = "androidx.core:core-ktx:${Versions.ANDROIDX_CORE}"
+    const val ANDROIDX_CONSTRAINT_LAYOUT =
+        "androidx.constraintlayout:constraintlayout:${Versions.ANDROID_MATERIAL}"
+
+    const val ANDROIDX_APPCOMPAT = "androidx.appcompat:appcompat:${Versions.ANDROIDX_APPCOMPAT}"
+
+    const val ANDROIDX_LIFECYCLE_LIVEDATA =
+        "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.ANDROIDX_LIFECYCLE}"
+    const val ANDROIDX_LIFECYCLE_VIEWMODEL =
+        "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.ANDROIDX_LIFECYCLE}"
+
+    const val ANDROIDX_ROOM_RUNTIME = "androidx.room:room-runtime:${Versions.ANDROIDX_ROOM}"
+    const val ANDROIDX_ROOM_KTX = "androidx.room:room-ktx:${Versions.ANDROIDX_ROOM}"
+    const val ANDROIDX_ROOM_COMPILER = "androidx.room:room-compiler:${Versions.ANDROIDX_ROOM}"
+
+    const val ANDROIDX_NAVIGATION_FRAGMENT_KTX =
+        "androidx.navigation:navigation-fragment-ktx:${Versions.ANDROIDX_NAVIGATION}"
+    const val ANDROIDX_NAVIGATION_UI_KTX =
+        "androidx.navigation:navigation-ui-ktx:${Versions.ANDROIDX_NAVIGATION}"
+
+    const val ANDROIDX_ACTIVITY_KTX = "androidx.activity:activity-ktx:${Versions.ANDROIDX_ACTIVITY}"
+
+    const val ANDROIDX_FRAGMENT_KTX = "androidx.fragment:fragment-ktx:${Versions.ANDROIDX_FRAGMENT}"
+
+    const val ANDROIDX_HILT_COMPILER = "androidx.hilt:hilt-compiler:${Versions.ANDROIDX_HILT}"
+
+    const val ANDROIDX_TEST_ESPRESSO_CORE =
+        "androidx.test.espresso:espresso-core:${Versions.ANDROIDX_ESPRESSO}"
+    const val ANDROIDX_ARCH_CORE_TESTING =
+        "androidx.test.espresso:espresso-core:${Versions.ANDROID_ARCH_CORE}"
+    const val ANDROIDX_TEST_JUNIT = "androidx.test.ext:junit:${Versions.ANDROIDX_TEST_JUNIT}"
+}
+
+object SquareUp {
+    const val RETROFIT = "com.squareup.retrofit2:retrofit:${Versions.RETROFIT}"
+    const val RETROFIT_GSON = "com.squareup.retrofit2:converter-gson:${Versions.RETROFIT}"
+    const val OKHTTP_LOGGING_INTERCEPTOR =
+        "com.squareup.okhttp3:logging-interceptor:${Versions.OKHTTP3}"
+}
+
+object Glide {
+    const val GLIDE = "com.github.bumptech.glide:glide:${Versions.GLIDE}"
+}
+
+object KotlinCoroutine {
+    const val ANDROID =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.KOTLIN_COROUTINE}"
+    const val CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.KOTLIN_COROUTINE}"
+    const val TEST = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.KOTLIN_COROUTINE}"
+}
+
+object Hilt {
+    const val HILT_ANDROID = "com.google.dagger:hilt-android:${Versions.HILT}"
+    const val HILT_ANDROID_COMPILER = "com.google.dagger:hilt-android-compiler:${Versions.HILT}"
+}
+
+object JUnit {
+    const val JUNIT = "junit:junit:${Versions.JUNIT}"
+}
